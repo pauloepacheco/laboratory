3b1a1baf3f86e47eec7967b26057667d889ef470
Eduardo <Eduardo@192.168.1.6>
2015-06-30 00:45:01 -0300

    added resource uri

---
+++ tcc-app/tcc-rest-api/src/main/java/com/br/ulbra/tcc/constants/RestApi.java
@@ -0,0 +1,6 @@
+package com.br.ulbra.tcc.constants;
+
+public interface RestApi {
+
+	static final String MY_RESOURCE_URI = "/myresource";
+}
--- tcc-app/tcc-rest-api/src/main/java/com/br/ulbra/tcc/rest/api/MyResource.java
+++ tcc-app/tcc-rest-api/src/main/java/com/br/ulbra/tcc/rest/api/MyResource.java
@@ -5,9 +5,11 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import com.br.ulbra.tcc.constants.RestApi;
+
 /** Example resource class hosted at the URI path "/myresource"
  */
-@Path("/myresource")
+@Path(RestApi.MY_RESOURCE_URI)
 public class MyResource {
     
     /** Method processing HTTP GET requests, producing "text/plain" MIME media
