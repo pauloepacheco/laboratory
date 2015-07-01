dcb6ac8ba64d7fd9d79d6543d9d251afef1fa7a1
Paulo Pacheco <eduardoguitar@gmail.com>
2015-06-30 22:21:18 -0300

    configuration changes

--- tcc-app/tcc-rest-api/src/main/java/com/br/ulbra/tcc/constants/RestApi.java
+++
@@ -1,6 +0,0 @@
-package com.br.ulbra.tcc.constants;
-
-public interface RestApi {
-
-	static final String MY_RESOURCE_URI = "/myresource";
-}
---
+++ tcc-app/tcc-rest-api/src/main/java/com/br/ulbra/tcc/rest/api/constants/URIResource.java
@@ -0,0 +1,6 @@
+package com.br.ulbra.tcc.rest.api.constants;
+
+public interface URIResource {
+
+	static final String MY_RESOURCE_URI = "/myresource";
+}
