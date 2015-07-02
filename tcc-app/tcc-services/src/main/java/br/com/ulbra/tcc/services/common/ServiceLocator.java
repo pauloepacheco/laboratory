a355a749821b1e612a768d79252c6e285979b468
Paulo Pacheco <eduardoguitar@gmail.com>
2015-07-02 00:38:44 -0300

    added support for log4j

--- tcc-app/tcc-services/src/main/java/br/com/ulbra/tcc/services/common/ServiceLocator.java
+++ tcc-app/tcc-services/src/main/java/br/com/ulbra/tcc/services/common/ServiceLocator.java
@@ -1,12 +1,13 @@
 package br.com.ulbra.tcc.services.common;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * The Service Locator Class
  * 
- * @author Eduardo
+ * @author Paulo Pacheco
  *
  */
 public class ServiceLocator {
@@ -15,18 +16,20 @@ public class ServiceLocator {
 	
 	private static final String SERVICES_CONFIG = "tcc-services.xml";
 	
+	private static final Logger LOGGER = Logger.getLogger(ServiceLocator.class);
+	
 	public static <T> T getServiceInstance(String beanName, Class<T> clazz) {
 		Object bean =  getInstance().getBean(beanName);
 		if(null == bean){
-			/*LOGGER.error("unable to locate service instance for the bean with name: " + beanName);
-			throw new EvolutionTechnicalException();*/
+			LOGGER.error("unable to locate service instance for the bean with name: " + beanName);
+			//TODO: throw Service Exception
 		}
 		
 		try{
 			return clazz.cast(bean);
 		}catch(ClassCastException ex){
-		/*	LOGGER.error("wrong  combination of bean name and class type passed, bean name: " + beanName + " class:" + clazz.getName());
-			throw new EvolutionTechnicalException();*/
+			LOGGER.error("wrong  combination of bean name and class type passed, bean name: " + beanName + " class:" + clazz.getName());
+			//TODO: throw Service Exception
 			return null;
 		}		
 	}
