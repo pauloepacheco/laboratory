69826e4ae45cd99014be391ed5dac4d7a1d8361d
Paulo Pacheco <eduardoguitar@gmail.com>
2015-07-03 00:50:16 -0300

    added jUnit configuration for common module

---
+++ tcc-app/tcc-common/src/test/java/br/com/ulbra/tcc/common/user/UserDaoTestImpl.java
@@ -0,0 +1,33 @@
+package br.com.ulbra.tcc.common.user;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.ulbra.tcc.common.dao.user.UserDao;
+import br.com.ulbra.tcc.common.entity.User;
+import br.com.ulbra.tcc.common.AbstractJUnitCommonTest;
+
+public class UserDaoTestImpl extends AbstractJUnitCommonTest {
+
+	@Autowired
+	private transient UserDao userDao;
+	
+	@Test
+	public void testFindUserById(){
+		User user = userDao.findUserById(new BigDecimal("2"));
+		
+		Assert.assertTrue(user != null);
+	}
+	
+	@Test
+	public void testGetAllRegisteredUsers(){
+		List<User> users = userDao.getAllRegisteredUsers();
+		
+		Assert.assertTrue(users != null && !users.isEmpty());
+	}
+
+}
