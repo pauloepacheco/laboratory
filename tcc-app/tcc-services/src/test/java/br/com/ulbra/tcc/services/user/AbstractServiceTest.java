da52c194291da95cbe3e571c7ced5f764b636b3b
Paulo Pacheco <eduardoguitar@gmail.com>
2015-07-02 21:36:32 -0300

    Changes to support new Junit AbstracServiceTest class

---
+++ tcc-app/tcc-services/src/test/java/br/com/ulbra/tcc/services/user/AbstractServiceTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.ulbra.tcc.services.user;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Paulo Pacheco
+ *
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:tcc-services.xml")
+public class AbstractServiceTest {
+
+}
--- tcc-app/tcc-services/src/test/java/br/com/ulbra/tcc/services/user/UserServiceImplTest.java
+++ tcc-app/tcc-services/src/test/java/br/com/ulbra/tcc/services/user/UserServiceImplTest.java
@@ -4,9 +4,6 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.junit.runner.RunWith;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,9 +18,7 @@ import br.com.ulbra.tcc.services.user.UserService;
  *
  */
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="classpath:tcc-services.xml")
-public class UserServiceImplTest{
+public class UserServiceImplTest extends AbstractServiceTest{
 
 	@Autowired
 	private transient UserService userService;
