096e4f16b850476884f8c8e9c818117a4f24e41c
Paulo Pacheco <eduardoguitar@gmail.com>
2015-08-23 15:34:35 -0300

    added userResource interface

--- tcc-app/tcc-rest-api/src/main/java/br/com/ulbra/tcc/restapi/resources/UserResource.java
+++ tcc-app/tcc-rest-api/src/main/java/br/com/ulbra/tcc/restapi/resources/UserResource.java
@@ -2,38 +2,16 @@ package br.com.ulbra.tcc.restapi.resources;
 
 import java.util.List;
 
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-
-import org.springframework.stereotype.Component;
-
 import br.com.ulbra.tcc.common.entity.User;
-import br.com.ulbra.tcc.restapi.constants.URIResourceBuilder;
-import br.com.ulbra.tcc.services.common.ServiceLocator;
-import br.com.ulbra.tcc.services.constants.ServiceBuilder;
-import br.com.ulbra.tcc.services.service.user.UserService;
 
 /**
- * The UserResource Class
+ * The UserResourceImpl Interface
  * 
  * @author Paulo Pacheco
  *
  */
 
-@Component
-@Path(URIResourceBuilder.UserResource.USER_URI)
-public class UserResource implements URIResourceBuilder {
-	
-	@GET
-	@Path(URIResourceBuilder.UserResource.GET_ALL_USERS_URI)
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<User> getAllRegisteredUsers(){
-				
-		UserService userService = ServiceLocator.
-				getServiceInstance(ServiceBuilder.USER_SERVICE, UserService.class);
-		
-		return userService.getListRegisteredUsers();
-	}
+public interface UserResource {
+
+	public List<User> getAllRegisteredUsers();
 }
---
+++ tcc-app/tcc-rest-api/src/main/java/br/com/ulbra/tcc/restapi/resources/UserResourceImpl.java
@@ -0,0 +1,39 @@
+package br.com.ulbra.tcc.restapi.resources;
+
+import java.util.List;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.springframework.stereotype.Component;
+
+import br.com.ulbra.tcc.common.entity.User;
+import br.com.ulbra.tcc.restapi.constants.URIResourceBuilder;
+import br.com.ulbra.tcc.services.common.ServiceLocator;
+import br.com.ulbra.tcc.services.constants.ServiceBuilder;
+import br.com.ulbra.tcc.services.service.user.UserService;
+
+/**
+ * The UserResourceImpl Class
+ * 
+ * @author Paulo Pacheco
+ *
+ */
+
+@Component
+@Path(URIResourceBuilder.UserResource.USER_URI)
+public class UserResourceImpl implements URIResourceBuilder {
+	
+	@GET
+	@Path(URIResourceBuilder.UserResource.GET_ALL_USERS_URI)
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<User> getAllRegisteredUsers(){
+				
+		UserService userService = ServiceLocator.
+				getServiceInstance(ServiceBuilder.USER_SERVICE, UserService.class);
+		
+		return userService.getListRegisteredUsers();
+	}
+}
